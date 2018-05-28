daa122b5c757f3080e2dde1e693cb06aeed5b12d
freebet <34800244+freebet@users.noreply.github.com>
2018-05-28 20:19:57 +0300

    Update globals.default.js

--- app/globals.default.js
+++ app/globals.default.js
@@ -5,7 +5,7 @@ angular.module('pool.globals', [])
 .factory('GLOBALS', function() {
 	return {
 		pool_name: "etnxpool.com",
-		api_url : 'http://165.227.152.155/api',
+		api_url : 'http://128.199.42.21/api',
 		api_refresh_interval: 5000,
 		app_update_interval: 30*60000
 	};
