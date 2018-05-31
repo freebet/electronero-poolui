0b397d519e3eaa489c39eb3c68480e137deb82dc
freebet <34800244+freebet@users.noreply.github.com>
2018-05-31 17:59:47 +0300

    Add files via upload

---
+++ deploy.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo "This assumes that you are doing a green-field install.  If you're not, please exit in the next 15 seconds."
+sleep 15
+echo "Continuing install, this will prompt you for your password if you're not already running as root and you didn't enable passwordless sudo.  Please do not run me as root!"
+if [[ `whoami` == "root" ]]; then
+    echo "You ran me as root! Do not run me as root!"
+    exit 1
+fi
+ROOT_SQL_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+CURUSER=$(whoami)
+sudo timedatectl set-timezone Etc/UTC
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+git clone https://github.com/freebet/paravan-poolui.git poolui
+cd poolui
+npm install
+./node_modules/bower/bin/bower update
+./node_modules/gulp/bin/gulp.js build
+cd build
+sudo ln -s `pwd` /var/www
+CADDY_DOWNLOAD_DIR=$(mktemp -d)
+cd $CADDY_DOWNLOAD_DIR
+curl -sL "https://snipanet.com/caddy.tar.gz" | tar -xz caddy init/linux-systemd/caddy.service
+sudo mv caddy /usr/local/bin
+sudo chown root:root /usr/local/bin/caddy
+sudo chmod 755 /usr/local/bin/caddy
+sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/caddy
+sudo groupadd -g 33 www-data
+sudo useradd -g www-data --no-user-group --home-dir /var/www --no-create-home --shell /usr/sbin/nologin --system --uid 33 www-data
+sudo mkdir /etc/caddy
+sudo chown -R root:www-data /etc/caddy
+sudo mkdir /etc/ssl/caddy
+sudo chown -R www-data:root /etc/ssl/caddy
+sudo chmod 0770 /etc/ssl/caddy
+sudo cp ~/poolui/caddyfile /etc/caddy/Caddyfile
+sudo chown www-data:www-data /etc/caddy/Caddyfile
+sudo chmod 444 /etc/caddy/Caddyfile
+sudo sh -c "sed 's/ProtectHome=true/ProtectHome=false/' init/linux-systemd/caddy.service > /etc/systemd/system/caddy.service"
+sudo chown root:root /etc/systemd/system/caddy.service
+sudo chmod 644 /etc/systemd/system/caddy.service
+sudo systemctl daemon-reload
+sudo systemctl enable caddy.service
+sudo systemctl start caddy.service
+rm -rf $CADDY_DOWNLOAD_DIR
